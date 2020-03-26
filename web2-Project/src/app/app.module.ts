d8bca6160862276aec405e8efb92354d6ff5d121
Andrej997 <andrej.km997@gmail.com>
2020-03-26 16:28:16 +0100

    Dodao
    Dodao sam 3 komponente
    1. fligth
    2. car
    3. log-in
    
    Klilkom na dugmad Flight ili Rent a car pozivaju se te komponente, cisto radi testranja sam dodao.
    Sva dalja logika ce se raditi iz te dve komponente.

--- web2-Project/src/app/app.module.ts
+++ web2-Project/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FlightComponent } from './components/flight/flight.component';
+import { CarComponent } from './components/car/car.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    FlightComponent,
+    CarComponent,
+    LogInComponent,
   ],
   imports: [
     BrowserModule,
