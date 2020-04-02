ef810f711fc9961831d61709651d5e8e1435cf3d
markomisojcic <49950468+markomisojcic@users.noreply.github.com>
2020-04-02 19:35:20 +0200

    Profile image reaguje na event i background za address collapse dodao
    Potrebne sitnije dorade

--- web2-Project/src/app/components/sign-in/sign-in.component.ts
+++ web2-Project/src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  @Input() source : string;
+
+  constructor() {
+    this.source = "../../../assets/img/person-icon.png";
+   }
 
   ngOnInit(): void {
   }
+
+  readURL(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.source = event.target.result.toString();
+        //alert(this.source);
+      }
+    }
+
+    // let files = event.target.files;
+    // if (files.length > 0) {
+    //   var reader = new FileReader();
+    //   reader.onload(files[0])
+    //   alert(files[0]);
+    // }
+    //var files = event.srcElement.files;
+
+    // let file = event.target.files[0];
+    // let fileName = file.name;
+    // console.log(file)
+    // console.log(fileName)
+    // let formData = new FormData();
+    // formData.append('file',file);
+    
+    //alert(Input);
+
+    this.source = "../../../assets/img/1.jpg";
+
+  }
+  // function readURL(input) {
+  //   if (input.files && input.files[0]) {
+  //       var reader = new FileReader();
+
+  //       reader.onload = function (e) {
+  //           $('#blah')
+  //               .attr('src', e.target.result);
+  //       };
+
+  //       reader.readAsDataURL(input.files[0]);
+  //   }
 
 
 }
