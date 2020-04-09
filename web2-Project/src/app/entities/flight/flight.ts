e00c54976496044aac476953668a487df19f2a19
Andrej997 <andrej.km997@gmail.com>
2020-04-09 10:50:21 +0200

    flight cards

--- web2-Project/src/app/entities/flight/flight.ts
+++ web2-Project/src/app/entities/flight/flight.ts
@@ -2,14 +2,17 @@ export class Flight {
     avioNama: string;
     from: string;
     to: string;
+    destImg: string;
     datumPolaska: Date;
     datumPovratka: Date;
     prise: number;
-
-    constructor(avioNama: string, from: string, to: string, datumPolaska: Date, datumPovratka: Date, prise: number) {
+    
+    constructor(avioNama: string, from: string, to: string, destImg: string,
+                datumPolaska: Date, datumPovratka: Date, prise: number) {
         this.avioNama = avioNama;
         this.from = from;
         this.to = to;
+        this.destImg = destImg;
         this.datumPolaska = datumPolaska;
         this.datumPovratka = datumPovratka;
         this.prise = prise;
